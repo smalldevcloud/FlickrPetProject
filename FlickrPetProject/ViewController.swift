b6cf0b269a600a77a854a5c605cc922b41a99d05
smalldevcloud <darkstormcloud8@gmail.com>
2023-12-16 16:42:24 +0300

    Initial Commit

---
+++ FlickrPetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrPetProject
+//
+//  Created by 8 on 16.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
